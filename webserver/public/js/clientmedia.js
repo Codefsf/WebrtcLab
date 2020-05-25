ee85a3d332f20e44c66b38fe4af356eb7a72c162
guoxudong <510105194@qq.com>
2020-05-25 19:17:24 +0800

    增加相机设备采集功能.

--- webserver/public/js/clientmedia.js
+++ webserver/public/js/clientmedia.js
@@ -1,15 +1,74 @@
 'use strict'
 
-var constraints = {video: true};
+var audioSource = document.querySelector('select#audioSource');
+var audioOutput = document.querySelector('select#audioOutput');
+var videoSource = document.querySelector('select#videoSource');
+
+var divConstraints = document.querySelector('div#constraints');
+
+var constraints = {video: true/*, audio:true*/};
 var videoPlayer = document.querySelector("video#player");
 
-function successCallback(stream){
-    console.log("success");
+function gotMediaStream(stream){
+    var videoTrack = stream.getVideoTracks()[0];
+    var videoConstraints = videoTrack.getSettings();
+
+    divConstraints.textContent = JSON.stringify(videoConstraints, null, 2);
+
+    window.stream = stream;
     videoPlayer.srcObject = stream;
+
+    return navigator.mediaDevices.enumerateDevices();
 }
 
-function errorCallback(stream){
-    console.log("error");
+function gotDevices(devicesInofs) {
+    deviceInfos.forEach(function(deviceinfo){
+
+		var option = document.createElement('option');
+		option.text = deviceinfo.label;
+		option.value = deviceinfo.deviceId;
+	
+		if(deviceinfo.kind === 'audioinput'){
+			audioSource.appendChild(option);
+		}else if(deviceinfo.kind === 'audiooutput'){
+			audioOutput.appendChild(option);
+		}else if(deviceinfo.kind === 'videoinput'){
+			videoSource.appendChild(option);
+		}
+	})
 }
 
-navigator.getUserMedia(constraints, successCallback, errorCallback);
+function handleError(stream){
+    
+    console.log("Get user media error");
+}
+
+
+function start() {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        console.log("This browser not support meidadevices");
+        return;
+    } else {
+        var deviceId = videoSource.value;
+        var constraints = {
+            video : {
+                width : 640,
+                height : 480,
+                frameRate : 15,
+                facingMode : 'enviroment',
+                deviceId : deviceId ? {exact:deviceId} : undefined
+            },
+
+            audio : false
+        }
+
+        navigator.mediaDevices.getUserMedia(constraints)
+                        .then(gotMediaStream)
+                        .then(gotDevices)
+                        .catch(handleError);
+
+    }
+
+}
+
+start();
