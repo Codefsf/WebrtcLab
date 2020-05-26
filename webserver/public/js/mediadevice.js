bbfbd4c772178c5ae53930dfee5deb3c4bb31ead
guoxudong <510105194@qq.com>
2020-05-26 21:15:06 +0800

    修改文件结构

--- webserver/public/js/mediadevice.js
+++ webserver/public/js/mediadevice.js
@@ -5,17 +5,8 @@ var audioOutput = document.querySelector('select#audioOutput');
 var videoSource = document.querySelector('select#videoSource');
 
 var divConstraints = document.querySelector('div#constraints');
-
-var btnRecord = document.querySelector('button#record');
-var btnPlay = document.querySelector('button#recplay');
-var btnDownload = document.querySelector('button#download');
-var recvideo = document.querySelector('video#recplayer');
-
-var constraints = {video: true/*, audio:true*/};
+var constraints = {video: true};
 var videoPlayer = document.querySelector("video#player");
-
-var buffer;
-var mediaRecorder;
 
 function gotMediaStream(stream){
     var videoTrack = stream.getVideoTracks()[0];
@@ -36,8 +27,6 @@ function gotDevices(devicesInfos) {
 		option.text = deviceinfo.label;
 		option.value = deviceinfo.deviceId;
         
-        console.log("--->" + deviceinfo.label + deviceinfo.deviceId + deviceinfo.kind);
-
 		if(deviceinfo.kind === 'audioinput'){
 			audioSource.appendChild(option);
 		}else if(deviceinfo.kind === 'audiooutput'){
@@ -77,74 +66,6 @@ function start() {
 
     }
 }
-
-function handleDataAvailable(e){
-	if(e && e.data && e.data.size > 0){
-	 	buffer.push(e.data);			
-	}
-}
-
-function startRecord()
-{
-    buffer = [];
-    var options = {
-        mimeType : "video/webm;codecs=vp8"
-    }
-
-    if(!MediaRecorder.isTypeSupported(options.mimeType)){
-		console.error(`${options.mimeType} is not supported!`);
-		return;	
-    }
-    
-    try{
-		mediaRecorder = new MediaRecorder(window.stream, options);
-	}catch(e){
-		console.error('Failed to create MediaRecorder:', e);
-		return;	
-	}
-
-	mediaRecorder.ondataavailable = handleDataAvailable;
-	mediaRecorder.start(10);
-}
-
-function stopRecord(){
-	mediaRecorder.stop();
-}
-
-btnRecord.onclick = ()=>{
-
-	if(btnRecord.textContent === 'Start Record'){
-		startRecord();	
-		btnRecord.textContent = 'Stop Record';
-		btnPlay.disabled = true;
-		btnDownload.disabled = true;
-	}else{
-		stopRecord();
-		btnRecord.textContent = 'Start Record';
-		btnPlay.disabled = false;
-		btnDownload.disabled = false;
-	}
-}
-
-btnPlay.onclick = ()=> {
-	var blob = new Blob(buffer, {type: 'video/webm'});
-	recvideo.src = window.URL.createObjectURL(blob);
-	recvideo.srcObject = null;
-	recvideo.controls = true;
-	recvideo.play();
-}
-
-btnDownload.onclick = ()=> {
-	var blob = new Blob(buffer, {type: 'video/webm'});
-	var url = window.URL.createObjectURL(blob);
-	var a = document.createElement('a');
-
-	a.href = url;
-	a.style.display = 'none';
-	a.download = 'aaa.webm';
-	a.click();
-}
-
 
 //Start capture
 start();
