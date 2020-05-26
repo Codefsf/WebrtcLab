418870781bb1eb015a46f06574a542cfeea8705c
guoxudong <510105194@qq.com>
2020-05-26 20:48:35 +0800

    添加录制功能

--- webserver/public/js/clientmedia.js
+++ webserver/public/js/clientmedia.js
@@ -6,28 +6,38 @@ var videoSource = document.querySelector('select#videoSource');
 
 var divConstraints = document.querySelector('div#constraints');
 
+var btnRecord = document.querySelector('button#record');
+var btnPlay = document.querySelector('button#recplay');
+var btnDownload = document.querySelector('button#download');
+var recvideo = document.querySelector('video#recplayer');
+
 var constraints = {video: true/*, audio:true*/};
 var videoPlayer = document.querySelector("video#player");
 
+var buffer;
+var mediaRecorder;
+
 function gotMediaStream(stream){
     var videoTrack = stream.getVideoTracks()[0];
-    var videoConstraints = videoTrack.getSettings();
+	var videoConstraints = videoTrack.getSettings();
+	
+	divConstraints.textContent = JSON.stringify(videoConstraints, null, 2);
 
-    divConstraints.textContent = JSON.stringify(videoConstraints, null, 2);
-
-    window.stream = stream;
-    videoPlayer.srcObject = stream;
+	window.stream = stream;
+	videoPlayer.srcObject = stream;
 
     return navigator.mediaDevices.enumerateDevices();
 }
 
-function gotDevices(devicesInofs) {
-    deviceInfos.forEach(function(deviceinfo){
+function gotDevices(devicesInfos) {
+    devicesInfos.forEach(function(deviceinfo){
 
 		var option = document.createElement('option');
 		option.text = deviceinfo.label;
 		option.value = deviceinfo.deviceId;
-	
+        
+        console.log("--->" + deviceinfo.label + deviceinfo.deviceId + deviceinfo.kind);
+
 		if(deviceinfo.kind === 'audioinput'){
 			audioSource.appendChild(option);
 		}else if(deviceinfo.kind === 'audiooutput'){
@@ -38,11 +48,9 @@ function gotDevices(devicesInofs) {
 	})
 }
 
-function handleError(stream){
-    
-    console.log("Get user media error");
+function handleError(error){
+    console.log("Get user media error:" + err);
 }
-
 
 function start() {
     if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
@@ -52,11 +60,11 @@ function start() {
         var deviceId = videoSource.value;
         var constraints = {
             video : {
-                width : 640,
-                height : 480,
+                width : 100,
+                height : 80,
                 frameRate : 15,
                 facingMode : 'enviroment',
-                deviceId : deviceId ? {exact:deviceId} : undefined
+                deviceId : deviceId ? {exact:deviceId} : undefined //TODO
             },
 
             audio : false
@@ -68,7 +76,75 @@ function start() {
                         .catch(handleError);
 
     }
-
 }
 
+function handleDataAvailable(e){
+	if(e && e.data && e.data.size > 0){
+	 	buffer.push(e.data);			
+	}
+}
+
+function startRecord()
+{
+    buffer = [];
+    var options = {
+        mimeType : "video/webm;codecs=vp8"
+    }
+
+    if(!MediaRecorder.isTypeSupported(options.mimeType)){
+		console.error(`${options.mimeType} is not supported!`);
+		return;	
+    }
+    
+    try{
+		mediaRecorder = new MediaRecorder(window.stream, options);
+	}catch(e){
+		console.error('Failed to create MediaRecorder:', e);
+		return;	
+	}
+
+	mediaRecorder.ondataavailable = handleDataAvailable;
+	mediaRecorder.start(10);
+}
+
+function stopRecord(){
+	mediaRecorder.stop();
+}
+
+btnRecord.onclick = ()=>{
+
+	if(btnRecord.textContent === 'Start Record'){
+		startRecord();	
+		btnRecord.textContent = 'Stop Record';
+		btnPlay.disabled = true;
+		btnDownload.disabled = true;
+	}else{
+		stopRecord();
+		btnRecord.textContent = 'Start Record';
+		btnPlay.disabled = false;
+		btnDownload.disabled = false;
+	}
+}
+
+btnPlay.onclick = ()=> {
+	var blob = new Blob(buffer, {type: 'video/webm'});
+	recvideo.src = window.URL.createObjectURL(blob);
+	recvideo.srcObject = null;
+	recvideo.controls = true;
+	recvideo.play();
+}
+
+btnDownload.onclick = ()=> {
+	var blob = new Blob(buffer, {type: 'video/webm'});
+	var url = window.URL.createObjectURL(blob);
+	var a = document.createElement('a');
+
+	a.href = url;
+	a.style.display = 'none';
+	a.download = 'aaa.webm';
+	a.click();
+}
+
+
+//Start capture
 start();
